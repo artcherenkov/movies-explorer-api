4d98451023a025dd905ab3aa2b6dc999eb13db70
Артем Черенков <art.cherenkov@gmail.com>
2021-08-26 01:59:05 +0700

    fix: fix typo

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,5 +49,5 @@ module.exports.createUser = (req, res, next) => {
 };
 module.exports.logout = (req, res) => {
   res.clearCookie("jwt");
-  res.status(201).send({ message: "Вы успешно вышли из своего аккауунта." });
+  res.status(201).send({ message: "Вы успешно вышли из своего аккаунта." });
 };
