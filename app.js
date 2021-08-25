a2e4724326844a46973ae1db9aa7bd6c999933af
Артем Черенков <art.cherenkov@gmail.com>
2021-08-25 23:27:17 +0700

    feat: добавить модели данных и  возможность подключения к бд

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
+const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/bitfilmsdb";
 
 const app = express();
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.get("/", (req, res) => {
   res.status(201).send({ message: "Hello world" });
---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: "Неправильный формат почты",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
