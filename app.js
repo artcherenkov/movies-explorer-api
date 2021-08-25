6b31464dbfcef550aa3da9191d02bff07553d846
Артем Черенков <art.cherenkov@gmail.com>
2021-08-26 00:35:02 +0700

    feat: добавить мидлвар auth и подключить bodyParser + cookieParser

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
+const auth = require("./middlewares/auth");
 const router = require("./routes");
+const NotFoundError = require("./errors/not-found");
+const UnauthorizedError = require("./errors/unauthorized");
 
 const PORT = process.env.PORT || 3000;
 const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/bitfilmsdb";
@@ -12,7 +18,17 @@ mongoose.connect(DB_URL, {
   useCreateIndex: true,
   useFindAndModify: false,
 });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/", router);
+
+app.use(auth, (req) => {
+  if (!req.user) {
+    throw new UnauthorizedError("Необходима авторизация");
+  }
+  throw new NotFoundError("Ресурс не найден");
+});
 
 app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
+const auth = require("../middlewares/auth");
 
 const authRouter = require("./auth");
 const moviesRouter = require("./movies");
 const usersRouter = require("./users");
 
-router.use("/users", usersRouter);
-router.use("/movies", moviesRouter);
+router.use("/users", auth, usersRouter);
+router.use("/movies", auth, moviesRouter);
 router.use("/", authRouter);
 
 module.exports = router;
