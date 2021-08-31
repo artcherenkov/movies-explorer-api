14db1cdc6b69a526e80514292130f08b4fd235d1
Артем Черенков <art.cherenkov@gmail.com>
2021-08-31 12:26:41 +0700

    fix: исправить схему валидации запросов на изменение данных пользователя

--- validation/users.js
+++ validation/users.js
@@ -1,10 +1,8 @@
 const { celebrate, Joi } = require('celebrate');
 
 module.exports.validateProfileData = celebrate({
-  body: Joi.object()
-    .min(1)
-    .keys({
-      email: Joi.string().email(),
-      name: Joi.string().min(2).max(30),
-    }),
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    name: Joi.string().min(2).max(30).required(),
+  }),
 });
